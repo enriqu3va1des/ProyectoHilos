a1533392cdc8c40943553b5ca8fb296353702099
enriquevaldes <luisen1211@gmail.com>
2022-11-07 21:22:02 -0600

    Proyecto

--- Avance Interfaz.py
+++ Avance Interfaz.py
@@ -6,87 +6,88 @@ import requests
 import threading
 
 
-def custom_hook(args):
+def custom(args):
     print(f'Thread failed: {args.exc_value}')
 
 
-class VentanaPrincipal(QMainWindow):
+class windowManager(QMainWindow): #creacion de la ventana
     def __init__(self):
         self.data_movies = []
         super().__init__()
-        self.setWindowTitle("Buscador LM")
-        self.resize(700, 280)
-        self.contenedor = QWidget()
+        self.setWindowTitle("Mi buscador")
+        self.resize(620, 255) #definir tamaño
+        self.container = QWidget()
         self.lyt_Images_Left = QVBoxLayout()
         self.lyt_Images_Center = QVBoxLayout()
         self.lyt_Images_Right = QVBoxLayout()
         self.lytPrincipal = QGridLayout()
-        self.lblBusca = QLabel("Nombre de la pelicula: ")
+        self.lblBusca = QLabel("words de películas a buscar: ")
         self.inlineText = QLineEdit()
-        self.btnBusca = QPushButton("Buscar")
-        self.btnBusca.clicked.connect(self.dividir_texto)
+        self.btnBusca = QPushButton("Filtrar")
+        self.btnBusca.clicked.connect(self.divTXT)
         self.lytPrincipal.addWidget(self.lblBusca, 0, 0)
         self.lytPrincipal.addWidget(self.inlineText, 0, 1)
         self.lytPrincipal.addWidget(self.btnBusca, 0, 2)
         self.lytPrincipal.addLayout(self.lyt_Images_Left, 1, 0)
         self.lytPrincipal.addLayout(self.lyt_Images_Center, 1, 1)
         self.lytPrincipal.addLayout(self.lyt_Images_Right, 1, 2)
-        self.contenedor.setLayout(self.lytPrincipal)
-        self.setCentralWidget(self.contenedor)
+        self.container.setLayout(self.lytPrincipal)
+        self.setCentralWidget(self.container)
 
 
-    def dividir_texto(self):
-        peliculas = []
-        palabras = []
-        index = 0
-        threading.excepthook = custom_hook
-        lista = self.inlineText.text()
-        for palabras in lista:
-            palabras = lista.split(",")
-        thread_lst = [threading.Thread(target=self.get_movie, args=(k, index)) for k in palabras]
+    def divTXT(self): #funcion para el control de el texto
+        movie = []
+        words = []
+        ind = 0
+        threading.excepthook = custom
+        list = self.inlineText.text()
+        for words in list: #ciclo flor para el uso de comas
+            words = list.split(",")
+        thread_lst = [threading.Thread(target=self.obMovie, args=(k, ind)) for k in words]
         for i in thread_lst:
             i.start()
-            index += 3
+            ind += 3
             print("Start")
         for i in thread_lst:
-            peliculas.append(i.join())
+            movie.append(i.join())
             print("Return")
-        self.resize(500, 650)
+        self.resize(750, 800) #defimir el tamaño
         print(self.data_movies)
-        self.draw_movies(self.data_movies, index)
+        self.drMovies(self.data_movies, ind)
 
-    def get_movie(self, palabra, index):
+    def obMovie(self, palabra, ind):
         url_service_image = "http://clandestina-hds.com:80/movies/title?search="
-        data_total = requests.get(url_service_image + palabra)
-        movies_data = data_total.json()
-        data_short = movies_data['results'][:3]
+        datosTotal = requests.get(url_service_image + palabra)
+        datosMovie = datosTotal.json() #aqui asignamos un valor a nuestra variable
+        data_short = datosMovie['results'][:3]
         for movie in data_short:
-            print("La pelicula de nombre: " + movie['title'])
-            self.data_movies.append(movie)
-        print(index)
+            print("Nombre de la pelicula: " + movie['title']) #mostrar el contenido de nuestro filtro
+            self.data_movies.append(movie) #asignacion por metodos
+            #imprimir el contenido de nuestro ind
+        print(ind)
 
-    def draw_movies(self, data_movies, index):
-        index = 0
+    def drMovies(self, data_movies, ind): #funcion de seguimiento para las peliculas
+        ind = 0
         for movie in data_movies:
             url_service_video = "https://clandestina-hds.com/movies/"
             data_video = requests.get(url_service_video + movie['id'])
             data2_video = data_video.json()
             url_video = data2_video['trailer']['linkEmbed']
             image = Poster(movie['image'], url_video)
-            info_movie = QTextEdit("Resumen: " + movie['plot'])
-            info_movie.setReadOnly(1)
-            if 0 <= index < 3:
+            txtMovie = QTextEdit("Resumen: " + movie['plot'])
+            txtMovie.setReadOnly(1)
+            if 0 <= ind < 3:
                 self.lyt_Images_Left.addWidget(image)
-                self.lyt_Images_Left.addWidget(info_movie)
-            if 3 <= index < 6:
+                self.lyt_Images_Left.addWidget(txtMovie)
+            if 3 <= ind < 6:
                 self.lyt_Images_Center.addWidget(image)
-                self.lyt_Images_Center.addWidget(info_movie)
-            if index >= 6:
+                self.lyt_Images_Center.addWidget(txtMovie)
+            if ind >= 6:
                 self.lyt_Images_Right.addWidget(image)
-                self.lyt_Images_Right.addWidget(info_movie)
-            index += 1
-        print(index)
-        self.setCentralWidget(self.contenedor)
+                self.lyt_Images_Right.addWidget(txtMovie)
+            ind += 1
+        print(ind)
+        self.setCentralWidget(self.container)
 
 
 class Poster(QLabel):
@@ -103,14 +104,14 @@ class Poster(QLabel):
         pixmap = pixmap.scaledToWidth(100)
         self.setPixmap(pixmap)
 
-    def mouseDoubleClickEvent(self, event):
+    def mouseDoubleClickEvent(self):
         if self.video_url is not None and self.video_url is not "":
             url = QUrl(self.video_url)
             QDesktopServices.openUrl(url)
 
 
 app = QApplication([])
-window = VentanaPrincipal()
+window = windowManager()
 window.show()
 
 app.exec_()
